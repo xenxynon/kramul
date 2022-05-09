3e20d059e9f92892e256bd2a9e8f4549afd11b6c
Mayur <flynryder427@gmail.com>
2022-05-09 20:02:09 +0530

    ok [skip ci]

--- build.sh
+++ build.sh
@@ -1,218 +1,78 @@
 #!/usr/bin/env bash
-
- #
- # Script For Building Android arm64 Kernel
- # 
- 
- # Specify Kernel Directory
-KERNEL_DIR="$(pwd)"
-
-# Zip Name
-ZIPNAME="Moonlight-Elaina"
-
-# Specify compiler ( eva , azure , proton , arter , aosp , atom x & nexus )
-COMPILER=proton
-
-# Device Name and Model
-DEVICE=lavender
-
-# Specify Version
-VERSION=V4.1
-
-# Specify Type
-if [ "$1" = "--qti" ]; then
-TYPE=Qti-Old
-elif [ "$1" = "--qti.12" ]; then
-TYPE=Qti-Old-12
-elif [ "$1" = "--old" ]; then
-TYPE=Old
-elif [ "$1" = "--new" ]; then
-TYPE=New
-echo "CONFIG_XIAOMI_NEWCAM=y" >> arch/arm64/configs/lavender-perf_defconfig
-fi
-
-# Kernel Defconfig
-DEFCONFIG=lavender-perf_defconfig
-
-# Linker
-LINKER=ld.lld
-
-# Path
+echo "Cloning dependencies"
+git clone --depth=1 https://github.com/sohamxda7/llvm-stable  clang
+git clone https://github.com/sohamxda7/llvm-stable -b gcc64 --depth=1 gcc
+git clone https://github.com/sohamxda7/llvm-stable -b gcc32  --depth=1 gcc32
+git clone --depth=1 https://github.com/sohamxda7/AnyKernel3 AnyKernel
+echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
-
-# Verbose Build
-VERBOSE=0
-
-# Kernel Version
-KERVER=$(make kernelversion)
-
-COMMIT_HEAD=$(git log --oneline -1)
-
-# Date and Time
-DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
+TANGGAL=$(date +"%F-%S")
 START=$(date +"%s")
-TANGGAL=$(date +"%F%S")
-
-FINAL_ZIP=${ZIPNAME}-${VERSION}-${TYPE}-${DEVICE}-${TANGGAL}.zip
-##----------------------------------------------------------##
-
-# Cloning Dependencies
-function clone() {
-    # Clone Toolchain
-        if [ $COMPILER = "azure" ]; then
-                post_msg " Cloning Azure Clang ToolChain "
-		git clone --depth=1 https://gitlab.com/xenxynon/azure-clang.git clang
-		PATH="${KERNEL_DIR}/clang/bin:$PATH"
-		elif [ $COMPILER = "atomx" ]; then
-		post_msg " Cloning Atom X Clang ToolChain "
-		git clone --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang.git clang
-		PATH="${KERNEL_DIR}/clang/bin:$PATH"
-		elif [ $COMPILER = "proton" ]; then
-		post_msg " Cloning Proton Clang ToolChain "
-		git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
-		PATH="${KERNEL_DIR}/clang/bin:$PATH"
-		elif [ $COMPILER = "nexus" ]; then
-		post_msg " Cloning Nexus Clang ToolChain "
-		git clone --depth=1 https://github.com/nexus-projects/nexus-clang.git clang
-		PATH="${KERNEL_DIR}/clang/bin:$PATH"
-		elif [ $COMPILER = "aosp" ]; then
-		post_msg " Cloning Aosp Clang 13.0.3 ToolChain "
-		git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r433403b.git -b 12.0 aosp-clang
-                git clone https://github.com/sohamxda7/llvm-stable -b gcc64 --depth=1 gcc
-                git clone https://github.com/sohamxda7/llvm-stable -b gcc32  --depth=1 gcc32
-                PATH="${KERNEL_DIR}/aosp-clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
-		elif [ $COMPILER = "arter" ]; then
-		post_msg " Cloning Arter GCC 9.3.0 ToolChain "
-		git clone --depth=1 -b gcc64 https://github.com/ImSpiDy/gcc-9.3.0 gcc64
-		git clone --depth=1 -b gcc32 https://github.com/ImSpiDy/gcc-9.3.0 gcc32
-		PATH=$KERNEL_DIR/gcc64/bin/:$KERNEL_DIR/gcc32/bin/:/usr/bin:$PATH
-		elif [ $COMPILER = "eva" ]; then
-		post_msg " Cloning Eva GCC ToolChain "
-		git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git gcc64
-		git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git gcc32
-		PATH=$KERNEL_DIR/gcc64/bin/:$KERNEL_DIR/gcc32/bin/:/usr/bin:$PATH
-        fi
-        # Clone AnyKernel3
-		git clone --depth=1 https://github.com/ImSpiDy/AnyKernel3 AnyKernel3
+KERNEL_DIR=$(pwd)
+PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
+export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+export ARCH=arm64
+export KBUILD_BUILD_HOST="cirrusci.org"
+export KBUILD_BUILD_USER="xenxynon"
+# sticker plox
+function sticker() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
+        -d sticker="CAACAgEAAxkBAAEnKnJfZOFzBnwC3cPwiirjZdgTMBMLRAACugEAAkVfBy-aN927wS5blhsE" \
+        -d chat_id=$chat_id
 }
-##------------------------------------------------------##
-
-function exports() {
-    if [ -d ${KERNEL_DIR}/clang ]; then
-    export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-    elif [ -d ${KERNEL_DIR}/aosp-clang ]; then
-    export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/aosp-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-    elif [ -d ${KERNEL_DIR}/gcc64 ]; then
-    export KBUILD_COMPILER_STRING=$("$KERNEL_DIR/gcc64"/bin/aarch64-elf-gcc --version | head -n 1)
-    fi
-    export ARCH=arm64
-    export SUBARCH=arm64
-    export LOCALVERSION="-${VERSION}-${TYPE}"
-    export KBUILD_BUILD_HOST=ArchLinux
-    export KBUILD_BUILD_USER="ImSpiDy"
-    export KBUILD_BUILD_VERSION=$DRONE_BUILD_NUMBER
-    export CI_BRANCH=$DRONE_BRANCH
-    export PROCS=$(nproc --all)
-
-}
-
-##----------------------------------------------------------------##
-
-if [ "$1" = "--old" ]; then
-function post_msg() {
+# Send info plox channel
+function sendinfo() {
     curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
         -d chat_id="$chat_id" \
-        -d sticker="CAACAgUAAxkBAAEEY19iTSpIgrbBsFleqpogLjvmLxR8EgACuAMAAlgiCVVYcVabvTjYQyME" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
-        -d text="$1"
+        -d text="<b>• Predator-Stormbreaker Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Xiaomi Redmi Note7/7S</b> (lavender)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>${KBUILD_COMPILER_STRING}</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#Stable"
 }
-fi 
-
-##----------------------------------------------------------##
-
+# Push kernel to channel
 function push() {
-    curl -F document=@$1 "https://api.telegram.org/bot$token/sendDocument" \
-         -F chat_id="$chat_id" \
-         -F "disable_web_page_preview=true" \
-         -F "parse_mode=html" \
-         -F caption="$2"
+    cd AnyKernel
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot${token}/sendDocument" \
+        -F chat_id="${chat_id}" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi Note 7/7s (lavender)</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
 }
-
-##----------------------------------------------------------##
-
+# Fin Error
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
+        -d chat_id="${chat_id}" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+    exit 1
+}
+# Compile plox
 function compile() {
-	post_msg "<b>$KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Kolkata date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0A<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><a href='$DRONE_COMMIT_LINK'>$COMMIT_HEAD</a>"
-	                        make O=out ARCH=arm64 ${DEFCONFIG}
-	                        if [ -d ${KERNEL_DIR}/clang ]; then
-	                        make -kj$(nproc --all) O=out \
-				ARCH=arm64 \
-				CC=clang \
-				CROSS_COMPILE=aarch64-linux-gnu- \
-				CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-				LD=${LINKER} \
-				AR=llvm-ar \
-				NM=llvm-nm \
-				OBJCOPY=llvm-objcopy \
-				OBJDUMP=llvm-objdump \
-				STRIP=llvm-strip \
-				READELF=llvm-readelf \
-				OBJSIZE=llvm-size \
-				V=$VERBOSE 2>&1 | tee error.log
-                                elif [ -d ${KERNEL_DIR}/gcc64 ]; then
-				make -kj$(nproc --all) O=out \
-				ARCH=arm64 \
-				CROSS_COMPILE_ARM32=arm-eabi- \
-				CROSS_COMPILE=aarch64-elf- \
-				LD=aarch64-elf-${LINKER} \
-				AR=llvm-ar \
-				NM=llvm-nm \
-				OBJCOPY=llvm-objcopy \
-				OBJDUMP=llvm-objdump \
-				STRIP=llvm-strip \
-				OBJSIZE=llvm-size \
-				V=$VERBOSE 2>&1 | tee error.log
-				elif [ -d ${KERNEL_DIR}/aosp-clang ]; then
-				make -kj$(nproc --all) O=out \
-				ARCH=arm64 \
-				CC=clang \
-				CLANG_TRIPLE=aarch64-linux-gnu- \
-				CROSS_COMPILE=aarch64-linux-android- \
-				CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-				LD=${LINKER} \
-				AR=llvm-ar \
-				NM=llvm-nm \
-				OBJCOPY=llvm-objcopy \
-				OBJDUMP=llvm-objdump \
-				STRIP=llvm-strip \
-				READELF=llvm-readelf \
-				OBJSIZE=llvm-size \
-				V=$VERBOSE 2>&1 | tee error.log
-				fi
-				
+    make O=out ARCH=arm64 lavender-perf_defconfig
+    make -j$(nproc --all) O=out \
+                    ARCH=arm64 \
+                    CC=clang \
+                    CLANG_TRIPLE=aarch64-linux-gnu- \
+                    CROSS_COMPILE=aarch64-linux-android- \
+                    CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
     if ! [ -a "$IMAGE" ]; then
-        push "error.log" "Build Throws Errors"
+        finerr
         exit 1
     fi
-    # Copy Files To AnyKernel3 Zip
-    cp $IMAGE AnyKernel3
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
-##----------------------------------------------------------##
-
+# Zipping
 function zipping() {
-    cd AnyKernel3 || exit 1
-    zip -r9 ${FINAL_ZIP} *
-    MD5CHECK=$(md5sum "$FINAL_ZIP" | cut -d' ' -f1)
-    push "$FINAL_ZIP" "Build took : $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s) | For <b>$MODEL ($DEVICE)</b> | <b>${KBUILD_COMPILER_STRING}</b> | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"
+    cd AnyKernel || exit 1
+    zip -r9 Predator-Stormbreaker-lavender-${TANGGAL}.zip *
     cd ..
 }
-##----------------------------------------------------------##
-
-clone
-exports
+sticker
+sendinfo
 compile
+zipping
 END=$(date +"%s")
 DIFF=$(($END - $START))
-zipping
-
-##----------------*****-----------------------------##
+push
